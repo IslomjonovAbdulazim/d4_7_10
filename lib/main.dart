089832672f98708eb584af025cea8612e0acb1b8
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-26 17:00:36 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
 
@@ -32,8 +33,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int cups = 0;
-  double value = 0;
+  int cups = 4;
+  double value = .5;
+  double prevValue = 0.5;
 
   @override
   Widget build(BuildContext context) {
@@ -49,12 +51,18 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(
                   height: 200,
                   width: 200,
-                  child: LiquidCircularProgressIndicator(
-                    backgroundColor: Color(0xffF4F8FB),
-                    valueColor: AlwaysStoppedAnimation(Color(0xff62CDFA)),
-                    borderWidth: 3,
-                    borderColor: Color(0xff62CDFA),
-                    value: value,
+                  child: TweenAnimationBuilder<double>(
+                    tween: Tween<double>(begin: prevValue, end: value),
+                    duration: Duration(milliseconds: 700),
+                    builder: (context, v, child) {
+                      return LiquidCircularProgressIndicator(
+                        backgroundColor: Color(0xffF4F8FB),
+                        valueColor: AlwaysStoppedAnimation(Color(0xff62CDFA)),
+                        borderWidth: 3,
+                        borderColor: Color(0xff62CDFA),
+                        value: v,
+                      );
+                    },
                   ),
                 ),
 
@@ -63,11 +71,33 @@ class _HomePageState extends State<HomePage> {
                 AnimatedFlipCounter(
                   value: cups,
                   suffix: " cups",
+                  duration: Duration(milliseconds: 1000),
                   textStyle: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.w700,
                     color: Color(0xff62CDFA),
                   ),
+                ),
+
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    CupertinoButton(
+                      color: Colors.red,
+                      onPressed: () {
+                        prevValue = value;
+                        cups = 0;
+                        value = 0;
+                        setState(() {});
+                      },
+                      borderRadius: BorderRadius.circular(100),
+                      padding: EdgeInsets.zero,
+                      child: Icon(
+                        CupertinoIcons.refresh_bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
